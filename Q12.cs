875fa5168656fca349dd71ff060761f1e2ffdf2e
aeomv <93030966+aeomv@users.noreply.github.com>
2023-03-01 17:16:50 +0500

    Create Q12.cs

---
+++ Q12.cs
@@ -0,0 +1,30 @@
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the number of elements to be stored in the array: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Element - {i} : ");
+                arr[i] = int.Parse(Console.ReadLine());
+
+            }
+                Array.Sort(arr);
+                Array.Reverse(arr);
+                Console.Write("Elements of array in sorted ascending order: ");
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write(arr[i] + " ");
+
+                }
+
+        }
+    }
+}
+//aeomv.com
